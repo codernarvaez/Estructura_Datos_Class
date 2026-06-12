44e5bdb7408f38b3baa7f263bc31c5f02a0c9434
Cristian Ramiro Narváez. <cristian.narvaez@unl.edu.ec>
2026-06-12 13:06:44 -0500

    Feat: Learning Javascript for noobscis

---
+++ Unidad02/00Javascript/tipos-datos.js
@@ -0,0 +1,35 @@
+//PRIMITIVOS
+let nombre = 'Maria'
+
+//NNUMERIDO
+let edad = 25
+
+//BOOLEANOS
+let esMayorDeEdad = true
+
+//null
+let noHayValor = null
+
+//undefined 
+let noDefinido = undefined
+
+//symbol valor unico
+let simboloUnico = Sumbol('único')
+
+// datos gigantes bigint
+let numeroGrande = 2n
+
+
+//COMPLETOS
+//object
+let carro ={
+    marca: 'Tesla',
+    modelo: 'Model S'
+}
+
+//arrays
+let frutas = ['manzana', 'pera', 'piña']
+
+
+//functions
+function saludar(){}
