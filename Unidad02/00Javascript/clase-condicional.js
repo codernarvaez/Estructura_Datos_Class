5bac9a777140c34c11a2d3d31ce2682735d715c4
Cristian Ramiro Narváez. <cristian.narvaez@unl.edu.ec>
2026-06-17 10:32:39 -0500

    class: 16062026 morning class

---
+++ Unidad02/00Javascript/clase-condicional.js
@@ -0,0 +1,15 @@
+/*
+Condicionales 
+
+
+*/
+
+let nombre = "Fernando"
+
+if (nombre === "Cristian") {
+    console.log("Hola Fer")
+}else if (nombre === "Cristian"){
+    console.log("Hola Cris")
+}else {
+    console.log("No encontre tu nombre")
+}
---
+++ Unidad02/00Javascript/clase-conversion.js
@@ -0,0 +1,16 @@
+const string = '47'
+const insteger = parseInt(string)
+console.log(typeof insteger)
+
+const stringDecimal = '3.14'
+const float = parseFloat(stringDecimal)
+console.log(stringDecimal)
+
+const binary = '10010'
+const decimal = parseInt(binary, 2)
+console.log(decimal)
+
+const suma = 3 - true
+console.log(suma)
+
+
