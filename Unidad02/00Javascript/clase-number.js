c97488fdb02535a0c20f454564950d36e1b16894
Cristian Ramiro Narváez. <cristian.narvaez@unl.edu.ec>
2026-06-16 22:57:09 -0500

    feat: clase del 06162026

---
+++ Unidad02/00Javascript/clase-number.js
@@ -0,0 +1,40 @@
+//Tipos de datos numericos
+// entero y decimal
+
+const entero = 42
+const decimal = 3.14
+console.log(typeof entero, typeof decimal)
+
+//Notacion científica
+const cientifico = 5e3
+
+//Representacion de otros contextos.
+const infinito = Infinity
+const noEsUnNumero = NaN
+
+
+//Operaciones Aritmenticas
+
+
+///Sumas REstas 
+
+const suma = 3 + 4
+const resta = 4 - 4
+const multiplicacion = 4 * 7 
+const division = 10 / 2
+
+//Modulps de exponenciacion
+const modulo = 15 % 8
+
+
+//Resultado de presicion
+const exponenciacion = 0.1 + 0.2
+console.log(exponenciacion)
+console.log(exponenciacion.toFixed(1))
+
+const raizcuadrada = Math.sqrt(16)
+const valorAbsoluto = Math.abs(-7)
+const aleatorio = Math.random()
+console.log(raizcuadrada)
+console.log(valorAbsoluto)
+console.log(aleatorio)
